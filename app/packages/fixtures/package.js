e41d56e5794471a7dd00af17f474dba28e7c5a31
Damir <damir@withpulp.com>
2015-08-16 02:53:15 -0400

    small pckg fix

--- app/packages/fixtures/package.js
+++ app/packages/fixtures/package.js
@@ -2,11 +2,11 @@ Package.describe({
   name: 'app:fixtures',
   version: '0.0.1',
   summary: 'Defines app fixtures and seed data',
-  //debugOnly: true
+  debugOnly: true
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('seed-data.js', 'server');
+  api.addFiles('seed_data.js', 'server');
   api.addFiles('fixtures.js', 'server');
 });
